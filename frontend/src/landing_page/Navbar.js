571c53d22269f74ae80a2d4a79f6ab13da9b19a8
Rupchand Naiya <rupchandnaiya27@gmail.com>
2026-09-21 21:12:22 +0530

    Update Navbar Dashboard link to live production Vercel URL

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const dashboardUrl = process.env.REACT_APP_DASHBOARD_URL || 
+      (window.location.hostname === "localhost" ? "http://localhost:3001" : "https://stocksphere-dashboard.vercel.app");
+
     return (
         <nav
-      class="navbar navbar-expand-lg border-bottom"
+      className="navbar navbar-expand-lg border-bottom"
       style={{ backgroundColor: "#FFF" }}
     >
-      <div class="container p-2">
-        <Link class="navbar-brand" to="/">
+      <div className="container p-2">
+        <Link className="navbar-brand" to="/">
           <img
             src="media/images/logo.png"
             style={{ width: "25%" }}
@@ -16,7 +19,7 @@ function Navbar() {
           />
         </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -24,43 +27,43 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <form class="d-flex" role="search">
-            <ul class="navbar-nav mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/signup">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="d-flex" role="search">
+            <ul className="navbar-nav mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/signup">
                   Signup
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/login">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/login">
                   Login
                 </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active font-weight-bold text-primary" href="http://localhost:3001">
+              <li className="nav-item">
+                <a className="nav-link active font-weight-bold text-primary" href={dashboardUrl}>
                   Dashboard
                 </a>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/about">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/about">
                   About
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/product">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/product">
                   Product
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/pricing">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/pricing">
                   Pricing
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link active" to="/support">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/support">
                   Support
                 </Link>
               </li>
